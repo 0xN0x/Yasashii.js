5b8af466616d1219762e8334c758198378ea308d
Shigehiro <mickael.courtiade@gmail.com>
2016-12-08 16:15:43 +0100

    Update yasashii.js

--- yasashii.js
+++ yasashii.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.set = require(__dirname+'/bot.json');
+bot.commands = fs.readdirSync(__dirname+'/commands/');
 
 global.bot = bot;
 global.ys = require(__dirname+'util.js');
@@ -11,6 +12,19 @@ bot.on('ready', function() {
   bot.user.setPresence({status: 'online', game:{ name:`${bot.set.prefix}help - ${bot.guilds.size} Servers`}});
   console.log(`${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers`);
   bot.start = moment().valueOf();
+});
+
+bot.on('message', function() {
+  if (m.author.bot === true) return;
+	if (m.channel.type === "dm") return;
+
+  if(m.content.startsWith(bot.set.prefix)) {
+    var command = m.content.split(" ")[0].replace(bot.set.prefix, "");
+    if (bot.commands.indexOf(command+'.js') > -1) {
+      var cmd = reload(__dirname+'/commands/'+command+'.js');
+      var args = m.content.substring((bot.set.prefix).length+command.length+1);
+    }
+  }
 });
 
 bot.login(bot.set.token);
