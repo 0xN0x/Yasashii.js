5c255d7298adab768851a233629f57093655175a
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-08 15:55:18 +0100

    Update yasashii.js

--- yasashii.js
+++ yasashii.js
@@ -1,1 +1,16 @@
+const moment = require('moment');
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
+bot.set = require(__dirname+'/bot.json');
+
+global.bot = bot;
+global.ys = require(__dirname+'util.js');
+
+bot.on('ready', function() {
+  bot.user.setPresence({status: 'online', game:{ name:`${bot.set.prefix}help - ${bot.guilds.size} Servers`}});
+  console.log(`${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers`);
+  bot.start = moment().valueOf();
+});
+
+bot.login(bot.set.token);
