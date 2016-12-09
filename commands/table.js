1abea62e6a8d3db43297507696b3839750ef10f3
Shigehiro <mickael.courtiade@gmail.com>
2016-12-09 10:28:46 +0100

    Update commands\table.js

--- commands/table.js
+++ commands/table.js
@@ -6,6 +6,6 @@ cmd.help = "Help !";
 cmd.args = '';
 
 cmd.run = (msg) => {
-  var args = m.content.substring((bot.set.prefix).length+m.content.split(" ")[0].replace(bot.set.prefix, "").length+1);
-  m.channel.sendMessage(args);
+  var args = msg.content.substring((bot.set.prefix).length+msg.content.split(" ")[0].replace(bot.set.prefix, "").length+1);
+  msg.channel.sendMessage(args);
 };
