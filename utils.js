445175269dcab9945b0561a48eb7a2a18bbd0499
Shigehiro <mickael.courtiade@gmail.com>
2016-12-09 16:29:11 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,3 +3,15 @@ var ys = module.exports = {};
 ys.logger = function(log) {
   console.log(log);
 };
+
+ys.send = function(channel, msg, file, embed) {
+  try {
+    if (!channel) return ys.logger('ERROR! : Need a channel.')
+    if (!msg) msg = '';
+    if (file) { channel.sendMessage(msg, file) } 
+    else if (toString.call(embed) === "[object Object]") { channel.sendMessage(msg, embed) }
+    else { channel.sendMessage(msg) }
+  } catch(err) {
+    console.log(err);
+  }
+};
