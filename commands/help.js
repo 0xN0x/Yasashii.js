8ab3cb31efdb615c748570e32da169366079618f
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-16 11:50:33 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,19 @@ cmd.command = true;
 cmd.help = "Help !";
 cmd.args = '';
 
-cmd.run = (msg) => {
-  ys.send(msg.channel, 'test.');
+cmd.run = (msg, args) => {
+  function format(file, help) {
+		var line = '.'+file.replace(".js", "")+"  = "+help+"";
+		return line;
+	}
+	var files = fs.readdirSync("./Modules/commands/");
+	var lines = ['**```css','[ Commands ]'];
+	files.forEach(function(file) {
+		var cmd = require("./"+file)
+		lines.push(format(file, cmd.help));
+	});
+  lines.push('```**');
+	var message = lines.join("\n");
+	m.author.sendMessage(message);
+	m.channel.sendMessage('All commands have been sent, look in your DM.');
 };
