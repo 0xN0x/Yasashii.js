fbe7c053b98f099892ddff260167752502bc175d
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-16 11:51:43 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,17 +7,17 @@ cmd.args = '';
 
 cmd.run = (msg, args) => {
   function format(file, help) {
-		var line = '.'+file.replace(".js", "")+"  = "+help+"";
-		return line;
-	}
-	var files = fs.readdirSync("./Modules/commands/");
-	var lines = ['**```css','[ Commands ]'];
-	files.forEach(function(file) {
-		var cmd = require("./"+file)
-		lines.push(format(file, cmd.help));
-	});
+    var line = '.'+file.replace(".js", "")+"  = "+help+"";
+    return line;
+  }
+  var files = fs.readdirSync("./Modules/commands/");
+  var lines = ['**```css','[ Commands ]'];
+  files.forEach(function(file) {
+    var cmd = require("./"+file)
+    lines.push(format(file, cmd.help));
+  });
   lines.push('```**');
-	var message = lines.join("\n");
-	m.author.sendMessage(message);
-	m.channel.sendMessage('All commands have been sent, look in your DM.');
+  var message = lines.join("\n");
+  m.author.sendMessage(message);
+  m.channel.sendMessage('All commands have been sent, look in your DM.');
 };
