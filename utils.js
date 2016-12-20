a022e0fa7f79d7b75f0e16425c1c0a4e0befe6fb
Shigehiro <mickael.courtiade@gmail.com>
2016-12-20 23:55:00 +0100

    [EDIT] Now can change the output file name.
    Add the posibility to change the output file name with ys.send function.

--- utils.js
+++ utils.js
@@ -4,12 +4,12 @@ ys.logger = function(log) {
   console.log(log);
 };
 
-ys.send = function(channel, msg, other) {
+ys.send = function(channel, msg, other, filename) {
   try {
     if (!channel) return ys.logger('ERROR! : Need a channel.')
     if (!msg) msg = '';
     if (other && toString.call(other) === "[object Object]") { return channel.sendMessage(msg, other) }
-    else if (other) { return channel.sendFile(msg, other) } 
+    else if (other) { return channel.sendFile(other, filename) }
     else { return channel.sendMessage(msg) }
   } catch(err) {
     console.log(err);
