364a0eecea11d470cc629d43b19240785ed1084a
Shigehiro <mickael.courtiade@gmail.com>
2016-12-20 23:28:10 +0100

    [EDIT] Change the osu! command help.

--- commands/osu.js
+++ commands/osu.js
@@ -10,7 +10,7 @@ var cmd = module.exports = {};
 
 cmd.type = "basic";
 cmd.command = true;
-cmd.help = "Get osu account informations. ex: $|osu [username] [basic|taiko|CTB|mania]";
+cmd.help = "Get osu account informations. ex: .osu <username> <basic|taiko|CTB|mania>";
 
 cmd.run = (msg, args) => {
 	msg.channel.sendFile('http://lemmmy.pw/osusig/sig.php?colour=hexffcc22&uname='+args.split(' ')[0]+'&mode='+mod[args.split(' ')[1]]+'&pp=0&flagshadow&flagstroke&darkheader&avatarrounding=4&onlineindicator=2&xpbar', 'osu.png')
