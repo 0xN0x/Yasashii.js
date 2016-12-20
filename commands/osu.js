21fbfa0593f14c1c7f1c5ed48a3c5033b992f4e3
Shigehiro <mickael.courtiade@gmail.com>
2016-12-20 23:41:53 +0100

    [EDIT] Fix osu! command and use the Yasashii utils for send the file.

--- commands/osu.js
+++ commands/osu.js
@@ -13,5 +13,5 @@ cmd.command = true;
 cmd.help = "Get osu account informations. ex: .osu <username> <basic|taiko|CTB|mania>";
 
 cmd.run = (msg, args) => {
-	msg.channel.sendFile('http://lemmmy.pw/osusig/sig.php?colour=hexffcc22&uname='+args.split(' ')[0]+'&mode='+mod[args.split(' ')[1]]+'&pp=0&flagshadow&flagstroke&darkheader&avatarrounding=4&onlineindicator=2&xpbar', 'osu.png')
+	ys.send(msg.channel, '', 'http://lemmmy.pw/osusig/sig.php?colour=hexffcc22&uname='+args.split(' ')[0]+'&mode='+mod[args.split(' ')[1]]+'&pp=0&flagshadow&flagstroke&darkheader&avatarrounding=4&onlineindicator=2&xpbar', 'osu.png')
 };
