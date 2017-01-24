6e9e4bf6e58410d2317818426809c57f526092e9
Shigehiro <Shiigehiro@users.noreply.github.com>
2017-01-24 18:45:24 +0100

    Correct a bug and add 2 utils functions

--- utils.js
+++ utils.js
@@ -1,17 +1,26 @@
 var ys = module.exports = {};
 
-ys.logger = function(log) {
-  console.log(log);
-};
+ys.logger = function(log) {console.log(log)};
+
+ys.warn = function(log) {console.warn(log)};
 
 ys.send = function(channel, msg, other, filename) {
   try {
     if (!channel) return ys.logger('ERROR! : Need a channel.')
-    if (!msg) msg = '';
+    if (!msg&&(other||filename)) msg = '';
     if (other && toString.call(other) === "[object Object]") { return channel.sendMessage(msg, other) }
     else if (other) { return channel.sendFile(other, filename) }
     else { return channel.sendMessage(msg) }
   } catch(err) {
     console.log(err);
   }
+};
+
+wolver.uptime = function(now, start) {
+  var stats = (now)-(start);
+  var stats_day = Math.floor((stats)/1000/60/60/24);
+  var stats_hour = Math.floor((stats-(stats_day*24*60*60*1000))/1000/60/60);
+  var stats_minute = Math.floor((stats-(stats_hour*60*60*1000))/1000/60);
+  var stats_second = Math.floor((stats-(stats_minute*60*1000))/1000);
+  return stats_day+"d "+stats_hour+"h "+stats_minute+"m "+stats_second+"s";
 };
